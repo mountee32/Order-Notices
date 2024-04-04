5c1b16fcc8fd865283f6996fccfca5cac35db819
mountee32 <124308058+mountee32@users.noreply.github.com>
2024-04-04 07:43:39 +0000

    shell with screens added

--- runme.sh
+++ runme.sh
@@ -6,26 +6,36 @@ REPO_DIR="/home/andy/Order-Notices"
 # Set the GitHub repository URL
 REPO_URL="https://github.com/mountee32/Order-Notices.git"
 
-while true; do
-		# Change to the repository directory
-		cd $REPO_DIR
+screen_name="order-notices"
 
-		# Check if the directory exists
-		if [ ! -d "$REPO_DIR" ]; then
-				# If the directory doesn't exist, clone the repository
-				git clone $REPO_URL $REPO_DIR
-		else
-				# If the directory exists, pull the latest changes
-				git pull
-		fi
-		chmod +x runme.sh
-		# Install required Python modules (just needed the first time)
-		# pip3 install -r requirements.txt
+if screen -list | grep -q "$screen_name"; then
+    echo "Screen session already exists, reattaching"
+    screen -r "$screen_name"
+else
+    echo "Screen session doesn't exist, creating new"
+    screen -dmS "$screen_name" bash -c '
+        while true; do
+            # Change to the repository directory
+            cd "$REPO_DIR"
 
-		# Run the Python program
-		python3 main.py
-		# python3 main.py excel
+            # Check if the directory exists
+            if [ ! -d "$REPO_DIR" ]; then
+                # If the directory doesn't exist, clone the repository
+                git clone "$REPO_URL" "$REPO_DIR"
+            else
+                # If the directory exists, pull the latest changes
+                git pull
+            fi
+            chmod +x runme.sh
+            # Install required Python modules (just needed the first time)
+            # pip3 install -r requirements.txt
 
-		# Sleep for 2 minutes (120 seconds)
-		sleep 120
-done
+            # Run the Python program
+            python3 main.py
+            # python3 main.py excel
+
+            # Sleep for 2 minutes (120 seconds)
+            sleep 120
+        done
+    '
+fi
