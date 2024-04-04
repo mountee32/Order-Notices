ad2a6658962bfce79ac6c54490898adaabeae19f
mountee32 <124308058+mountee32@users.noreply.github.com>
2024-04-04 06:46:17 +0000

    with the runme shell script

---
+++ runme.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set the directory where you want to clone the repository
+REPO_DIR="/root/Order-Notices"
+
+# Set the GitHub repository URL
+REPO_URL="https://github.com/mountee32/Order-Notices.git"
+
+while true; do
+		# Change to the repository directory
+		cd $REPO_DIR
+
+		# Check if the directory exists
+		if [ ! -d "$REPO_DIR" ]; then
+				# If the directory doesn't exist, clone the repository
+				git clone $REPO_URL $REPO_DIR
+		else
+				# If the directory exists, pull the latest changes
+				git pull
+		fi
+
+		# Install required Python modules (just needed the first time)
+		# pip3 install -r requirements.txt
+
+		# Run the Python program
+		python3 main.py
+		# python3 main.py excel
+
+		# Sleep for 2 minutes (120 seconds)
+		sleep 120
+done
